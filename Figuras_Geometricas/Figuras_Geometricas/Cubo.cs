32fe7ebdfbd3b5e527793db42cfa5a795becf085
SebastianGP4 <163374024+SebastianGP4@users.noreply.github.com>
2024-05-22 18:32:10 -0600

    Add files via upload

---
+++ Figuras_Geometricas/Figuras_Geometricas/Cubo.cs
@@ -0,0 +1,36 @@
+﻿using FiguraGeometrica;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Figuras_Geometricas
+{
+    class Cubo : Figura
+    {
+        //Usamos el constructor de la clase padre
+        public Cubo(float lado1)
+        {
+            Lado1 = lado1; //radio se va a leer de la caja de texto con usando el boton guardar
+        }
+        //vamos a usar los métodos de la clase padre
+        //vamos a sobreesribir el comportamiento de estos
+        public override float area()
+        {
+            return 6 * Lado1 * Lado1; //AGREGAMOS F AL FINAL DE UN NUMERO CUANDO ES UNA CONSTANTE NO DEFINIDA EN UNA VARIABLE PREVIAMENTE, ASI EL PROGRAMA SABE QUE ES UN #
+        }
+
+        public override float perimetro()
+        {
+            throw new NotImplementedException();
+            //ESTO ES UNA EXEPCION DE USO DEFAULT DEL SISTEMA
+        }
+
+        public override float volumen()
+        {
+            return Lado1 * Lado1 * Lado1;
+        }
+    }
+}
---
+++ Figuras_Geometricas/Figuras_Geometricas/Esfera.cs
@@ -0,0 +1,36 @@
+﻿using FiguraGeometrica;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Figuras_Geometricas
+{
+    class Esfera : Figura //Esta es una clase hija de figura
+    {
+        //Usamos el constructor de la clase padre
+        public Esfera(float radio) 
+        {
+            Lado1 = radio; //radio se va a leer de la caja de texto con usando el boton guardar
+        }
+        //vamos a usar los métodos de la clase padre
+        //vamos a sobreesribir el comportamiento de estos
+        public override float area()
+        {
+            return 4 * 3.1416F * Lado1 * Lado1; //AGREGAMOS F AL FINAL DE UN NUMERO CUANDO ES UNA CONSTANTE NO DEFINIDA EN UNA VARIABLE PREVIAMENTE, ASI EL PROGRAMA SABE QUE ES UN #
+        }
+
+        public override float perimetro()
+        {
+            throw new NotImplementedException();
+            //ESTO ES UNA EXEPCION DE USO DEFAULT DEL SISTEMA
+        }
+
+        public override float volumen()
+        {
+            return 4 / 3 * 3.1416F * Lado1 * Lado1 * Lado1;
+        }
+    }
+}
